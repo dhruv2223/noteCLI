2e9e2c65f2f49b797359a6c94d095487c33e97c4
dhruv2223 <DHRUV.CHOTALIA017@svkmmumbai.onmicrosoft.com>
2024-08-07 23:32:32 +0530

    final

---
+++ src/command.js
@@ -0,0 +1,84 @@
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import {newNote,getAllNotes,findNotes,removeNotes,removeAllNotes} from "./notes.js";
+const listNotes = (notes) => {
+  notes.forEach(({id,content,tags})=>{
+    console.log('id: ',id);
+    console.log('tags :',tags);
+    console.log('content :',content)
+    console.log('\n');
+  })
+}
+const wait = (time,cb)=>{
+  setTimeout(() => {
+    cb();
+  },time);
+}
+yargs(hideBin(process.argv))
+  .command('new <note>','create a new note',yargs=>{
+    return yargs.positional('note',{
+      type:'string',
+      description:'The content of the note to create', 
+    })
+  },async (argv)=>{
+      const tags = argv.tags ? argv.tags.split(',') : [];
+      const note = await newNote(argv.note,tags);
+      console.log("New note!",note);
+
+   })
+  .option('tags',{
+    alias:'t',
+    type:'string',
+    description:'tags to add to the notes'
+  })
+.command('all', 'get all notes', () => {}, async (argv) => {
+    const notes = await getAllNotes();
+    listNotes(notes);
+    
+  })
+  .command('find <filter>', 'get matching notes', yargs => {
+    return yargs.positional('filter', {
+      describe: 'The search term to filter notes by, will be applied to note.content',
+      type: 'string'
+    })
+  }, async (argv) => {
+      const matches = await findNotes(argv.filter);
+      listNotes(matches);
+  })
+  .command('remove <id>', 'remove a note by id', yargs => {
+    return yargs.positional('id', {
+      type: 'number',
+      description: 'The id of the note you want to remove'
+    })
+  }, async (argv) => {
+      const id = await removeNotes(argv.id);
+      console.log(id); 
+  })
+  .command('clean', 'remove all notes', () => {}, async (argv) => {
+    await removeAllNotes();
+    console.log("db reset done");
+  })
+  .demandCommand(1)
+  .parse()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
