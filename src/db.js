5d94b66c3e647d254980fcd78031e315ea249324
dhruv2223 <DHRUV.CHOTALIA017@svkmmumbai.onmicrosoft.com>
2025-01-10 15:38:36 +0530

    fs changes

--- src/db.js
+++ src/db.js
@@ -1,16 +1,20 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-const DB_PATH = path.resolve(
-  new URL("../db.json", import.meta.url).pathname.replace(/^\/+/, ""),
-);
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const DB_PATH = path.resolve(__dirname, "../db.json");
+
 export const getDB = async () => {
   const db = await fs.readFile(DB_PATH, "utf-8");
   return JSON.parse(db);
 };
+
 export const saveDB = async (db) => {
   await fs.writeFile(DB_PATH, JSON.stringify(db, null, 2));
   return db;
 };
+
 export const insertDB = async (note) => {
   const db = await getDB();
   db.notes.push(note);
